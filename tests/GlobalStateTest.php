044776a3ec2570fa8d9630632905327e3555ce25
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-04-29 12:53:40 +0200

    GlobalState

---
+++ tests/GlobalStateTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use WyriHaximus\React\Inspector\F42\GlobalState;
+
+final class GlobalStateTest extends TestCase
+{
+    public function testGlobalState()
+    {
+        self::assertSame([],GlobalState::get());
+        GlobalState::set('key', 1);
+        self::assertSame(['key' => 1],GlobalState::get());
+        GlobalState::incr('key');
+        self::assertSame(['key' => 2],GlobalState::get());
+        GlobalState::incr('key', 3);
+        self::assertSame(['key' => 5],GlobalState::get());
+        GlobalState::reset();
+        self::assertSame([],GlobalState::get());
+        GlobalState::incr('key', 3);
+        self::assertSame(['key' => 3],GlobalState::get());
+    }
+}
