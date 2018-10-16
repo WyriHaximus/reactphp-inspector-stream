1391a3bd29ca0bce98f3deb97e976278875123a8
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-10-16 22:15:25 +0200

    Added which function it was testing in case it fails

--- tests/MonkeyTest.php
+++ tests/MonkeyTest.php
@@ -11,8 +11,8 @@ final class MonkeyTest extends TestCase
     public function testRerouting(): void
     {
         $code = (new PhpParserPrinter())(Monkey::patch(MonkeyPatchTestTarget::class));
-        self::assertNotFalse(strpos($code, '\React\Stream\fwrite'));
-        self::assertNotFalse(strpos($code, '\React\Stream\fread'));
-        self::assertNotFalse(strpos($code, '\React\Stream\stream_get_contents'));
+        self::assertNotFalse(strpos($code, '\React\Stream\fwrite'), 'fwrite');
+        self::assertNotFalse(strpos($code, '\React\Stream\fread'), 'fread');
+        self::assertNotFalse(strpos($code, '\React\Stream\stream_get_contents'), 'stream_get_contents');
     }
 }
